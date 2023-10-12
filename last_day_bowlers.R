b768b1241b0a6bae90894acd582e9a6d4b3fa01d
jimboirish <147644052+jimboirish@users.noreply.github.com>
2023-10-12 13:56:23 -0400

    Add files via upload
    last day stuff

---
+++ last_day_bowlers.R
@@ -0,0 +1,51 @@
+library(dplyr)
+library(DBI)
+library(dbplyr)
+library(odbc)
+
+odbcListDrivers()
+
+con <- DBI::dbConnect(odbc(),
+                      Driver = "ODBC Driver 17 for SQL Server",
+                      Server = "mcobsql.business.nd.edu",
+                      UID = "MSBAstudent",
+                      PWD = "SQL%database!Mendoza",
+                      Port = 3306, 
+                      Database = "BowlingLeagueExample")
+
+dbListFields(con, "Bowler_Scores")
+
+dbListFields(con, "Bowlers")
+
+select_q <- dbSendQuery(
+  conn = con, 
+  statement = "SELECT * FROM Bowler_Scores"
+)
+
+select_res <- dbFetch(select_q)
+
+library(ggplot2)
+
+select_res$HandiCap = select_res$HandiCapScore - select_res$RawScore
+
+ggplot(select_res, aes(x=RawScore, y =HandiCapScore, color = WonGame))+
+  geom_point()+
+  geom_smooth(method = lm, color = "black")+
+  theme_minimal()
+
+ggplot(select_res, aes(x=HandiCap, y =RawScore, color = WonGame))+
+  geom_point()+
+  geom_smooth(method = lm, se = FALSE, color = "black")+
+  theme_minimal()
+
+
+regr <- lm(RawScore ~ HandiCap, data=select_res)
+
+
+
+cor(select_res$RawScore, select_res$HandiCapScore)
+
+  
+
+dbClearResult(select_q)
+
